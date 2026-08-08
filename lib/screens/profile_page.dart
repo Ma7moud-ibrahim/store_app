9903ebb2efcaea35e5e311585f956b18211ddf74
Ma7moud-ibrahim <ahmed.01090223081@example.com>
2026-08-08 18:09:48 +0300

    update profile page add the editProfile

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,28 +1,117 @@
+import 'package:app_store/widget/show_edit_profile_dialogs.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
-  static final id = 'ProfilePage';
+
+  static final String id = 'ProfilePage';
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String nameUser = 'Mahmoud Ibrahim';
+  String phoneUser = '010xxxxxxxxxx';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('More')),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 40,
+      appBar: AppBar(
+        title: const Text(
+          'More',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+        ),
+      ),
 
-                backgroundImage: NetworkImage(
-                  'https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, top: 30, bottom: 30),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 32,
+                    backgroundImage: NetworkImage(
+                      'https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp',
+                    ),
+                  ),
+
+                  const SizedBox(width: 14),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        nameUser,
+                        style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+
+                      Text(
+                        phoneUser,
+                        style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              customListTileProfile(
+                icon: const Icon(
+                  Icons.edit,
+                  size: 35,
+                  color: Colors.blueAccent,
                 ),
+                textName: const Text(
+                  'Edit Profile',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return ShowEditProfileDialogs(
+                        nameUser: nameUser,
+                        phoneUser: phoneUser,
+                        onUpdate: onUpdate,
+                      );
+                    },
+                  );
+                },
               ),
             ],
           ),
-        ],
+        ),
       ),
+    );
+  }
+
+  Padding customListTileProfile({
+    required Icon icon,
+    required Text textName,
+    required VoidCallback onTap,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 40),
+      child: ListTile(leading: icon, title: textName, onTap: onTap),
+    );
+  }
+
+  void onUpdate(String name, String phone) {
+    setState(() {
+      nameUser = name;
+      phoneUser = phone;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Profile updated successfully')),
     );
   }
 }
---
+++ lib/widget/show_edit_profile_dialogs.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class ShowEditProfileDialogs extends StatefulWidget {
+  const ShowEditProfileDialogs({
+    super.key,
+    required this.nameUser,
+    required this.phoneUser,
+    required this.onUpdate,
+  });
+
+  final String nameUser;
+  final String phoneUser;
+
+  final Function(String name, String phone) onUpdate;
+
+  @override
+  State<ShowEditProfileDialogs> createState() => _ShowEditProfileDialogsState();
+}
+
+class _ShowEditProfileDialogsState extends State<ShowEditProfileDialogs> {
+  late TextEditingController nameController;
+  late TextEditingController phoneController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    nameController = TextEditingController(text: widget.nameUser);
+
+    phoneController = TextEditingController(text: widget.phoneUser);
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Profile'),
+
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(labelText: 'Enter your name'),
+          ),
+
+          const SizedBox(height: 15),
+
+          TextField(
+            controller: phoneController,
+            keyboardType: TextInputType.phone,
+            decoration: const InputDecoration(labelText: 'Enter your phone'),
+          ),
+        ],
+      ),
+
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Cancel'),
+        ),
+
+        ElevatedButton(
+          onPressed: () {
+            widget.onUpdate(nameController.text, phoneController.text);
+
+            Navigator.pop(context);
+          },
+          child: const Text('Update'),
+        ),
+      ],
+    );
+  }
+
+  void onUpdate(String name, String phone) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Profile updated successfully')),
+    );
+  }
+}
